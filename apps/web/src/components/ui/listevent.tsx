691ec0ab4740cad0f95d9483dc67510bacdf970e
Anissong <dksadsk>
2024-02-01 00:43:22 +0700

    Pagination

--- apps/web/src/components/ui/listevent.tsx
+++ apps/web/src/components/ui/listevent.tsx
@@ -5,9 +5,13 @@ import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
+import Pagination from './pagination';
 
 const Listevent = () => {
   const [dataBlog, setDataBlog] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(4);
+
   useEffect(() => {
     getProducts();
   }, []);
@@ -18,14 +22,23 @@ const Listevent = () => {
     console.log(dataBlog.title);
   };
 
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = dataBlog.slice(firstPostIndex, lastPostIndex);
+
+  let pages = [];
+  for (let i = 1; i <= Math.ceil(dataBlog.length / postsPerPage); i++) {
+    pages.push(i);
+  }
+
   return (
     <>
       <div className="px-[6rem] ">
         <h1 className="text-black font-bold text-[30px] lg:text-[40px] mb-[2rem]">
-          Upcoming<span className="text-[#7848F4] ">Event</span>
+          Upcoming<span className="text-[#7848F4] ">Event </span>
         </h1>
         <div className="grid-cols-4 grid gap-[3rem]  items-center justify-center">
-          {dataBlog.map((product) => (
+          {currentPosts.map((product) => (
             // eslint-disable-next-line react/jsx-key
             <Link href={`Landing/detailsEvent/${product.id}`}>
               <div
@@ -51,6 +64,22 @@ const Listevent = () => {
             </Link>
           ))}
         </div>
+        <div className="flex gap-2 justify-center mb-[5rem]">
+          {pages.map((page: any, index) => (
+            <button
+              key={index}
+              onClick={() => setCurrentPage(page)}
+              className="border-none bg-[#7848F4] p-2 text-white text-[1.2rem] rounded-md cursor-pointer hover:shadow-md hover:bg-[#6039c3]"
+            >
+              {page}
+            </button>
+          ))}
+        </div>
+        {/* <Pagination
+          totalPosts={dataBlog.length}
+          postsPerPage={postsPerPage}
+          setCurrentPage={setCurrentPage}
+        /> */}
       </div>
     </>
   );
