557c889919278c41c55d0d979930d6519adfbb8c
Malvin A <malvinaudriansyah77@gmail.com>
2024-02-01 13:48:14 +0700

    feat: prevents visitor without token from accessing Dashboard

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -11,14 +11,25 @@ import {
 } from '@/components/ui/card';
 
 import DashboardSidebar from '@/components/DashboardSidebar';
+import BarChart from '@/components/chart/BarChart';
+import { useRouter } from 'next/navigation';
 
 export default function DashboardPage() {
+  const router = useRouter();
+
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    alert('You need to Login before you can access Dashboard');
+    router.push('/login');
+  }
+
   return (
     <>
       <div className="flex flex-row">
         <DashboardSidebar />
-        <div className="p-4 w-full">
-          <div className="flex flex-row justify-around">
+        <div className="flex flex-col p-4 max-w-[1400px] mx-auto">
+          <div className="flex flex-row gap-5 justify-between mb-5">
             <Card className="max-w-[300px]">
               <CardHeader>
                 <CardTitle>Today's Income</CardTitle>
@@ -26,7 +37,6 @@ export default function DashboardPage() {
               <CardContent>
                 <p>Rp. 250.000,-</p>
               </CardContent>
-              <hr></hr>
               <CardFooter className="flex items-center">
                 <p>
                   <span className="text-[#4CAF55] font-bold">55%</span> than
@@ -50,21 +60,21 @@ export default function DashboardPage() {
             </Card>
             <Card className="max-w-[300px]">
               <CardHeader>
-                <CardTitle>Today's Income</CardTitle>
+                <CardTitle>Event Organized</CardTitle>
               </CardHeader>
               <CardContent>
-                <p>Rp. 250.000,-</p>
+                <p>4</p>
               </CardContent>
               <CardFooter>
                 <p>
-                  <span className="text-[#4CAF55] font-bold">55%</span> than
-                  last week
+                  <span className="text-[#4CAF55] font-bold">30%</span> than
+                  last year
                 </p>
               </CardFooter>
             </Card>
             <Card className="max-w-[300px]">
               <CardHeader>
-                <CardTitle>Today's Income</CardTitle>
+                <CardTitle>Total Profit</CardTitle>
               </CardHeader>
               <CardContent>
                 <p>Rp. 250.000,-</p>
@@ -77,6 +87,14 @@ export default function DashboardPage() {
               </CardFooter>
             </Card>
           </div>
+          <Card className="">
+            <CardHeader>
+              <CardTitle>This Week's Profit</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <BarChart />
+            </CardContent>
+          </Card>
         </div>
       </div>
     </>
--- apps/web/src/components/DashboardSidebar.tsx
+++ apps/web/src/components/DashboardSidebar.tsx
@@ -63,7 +63,7 @@ export default function DashboardSidebar() {
             </li>
             <li>
               <Link
-                href="/"
+                href="/Landing"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <FaHome size={25} className="text-gray-400" />
